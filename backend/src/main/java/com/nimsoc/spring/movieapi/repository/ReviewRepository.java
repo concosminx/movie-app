a1eb19f7a41faf6b0644d5839907a8056e8948ba
Cosmin Constantin <concosminx@users.noreply.github.com>
2023-09-06 17:55:23 +0300

    App from tutorial refactored for gradle

---
+++ backend/src/main/java/com/nimsoc/spring/movieapi/repository/ReviewRepository.java
@@ -0,0 +1,10 @@
+package com.nimsoc.spring.movieapi.repository;
+
+import com.nimsoc.spring.movieapi.model.Review;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
---
+++ backend/src/main/java/com/nimsoc/spring/movieapi/service/ReviewService.java
@@ -0,0 +1,33 @@
+package com.nimsoc.spring.movieapi.service;
+
+import com.nimsoc.spring.movieapi.model.Movie;
+import com.nimsoc.spring.movieapi.model.Review;
+import com.nimsoc.spring.movieapi.repository.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Service
+public class ReviewService {
+    @Autowired
+    private ReviewRepository repository;
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public Review createReview(String reviewBody, String imdbId) {
+        Review review = repository.insert(new Review(reviewBody, LocalDateTime.now(), LocalDateTime.now()));
+
+        mongoTemplate.update(Movie.class)
+            .matching(Criteria.where("imdbId").is(imdbId))
+            .apply(new Update().push("reviews").value(review))
+            .first();
+
+        return review;
+    }
+}
